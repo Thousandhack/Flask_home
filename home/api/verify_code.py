6682b5bb0ac6a7a81002b38c6004675a9dd11d70
Thousandhack <1391053697@qq.com>
2020-07-28 22:50:51 +0800

    实现生成短信验证码接口

--- home/api/verify_code.py
+++ home/api/verify_code.py
@@ -1,11 +1,16 @@
 from . import api
 from home.utils.captcha.captcha import captcha
-from home import redis_store   # 导入redis 实例
 from home.utils import constants
 from home.utils.response_code import RET
+from home import redis_store  # 导入redis 实例
+from home import db
+from home.libs.yuntongxun.sms import CCP
+from home.models import User
 from flask import current_app
 from flask import make_response
+from flask import request
 from flask import jsonify
+import random
 
 
 @api.route("/image_codes/<image_code_id>")
@@ -41,3 +46,66 @@ def get_iamge_code(image_code_id):
     resp = make_response(image_data)
     resp.headers["Content-Type"] = "image/jpg"
     return resp
+
+
+# GET /api/v1.0/sms_codes/<mobile>?image_code=xxxx&image_code_id=xxxx
+@api.route("/sms_codes/<re(r'1[34578]\d{9}'):mobile>")
+def get_sms_code(mobile):
+    """
+    获取短信验证码
+    :return:
+    """
+    # 获取参数
+    image_code = request.args.get("image_code")
+    image_code_id = request.args.get("image_code_id")
+    # 校验
+    if not all([image_code, image_code_id]):
+        # 表示参数不完整
+        return jsonify(errno=RET.PARAMERR, errmsg="请求参数错误！")
+    # 业务逻辑处理
+    # 从redis中取出真实的图片验证码
+    try:
+        real_image_code = redis_store.get("image_code_%s" % image_code_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        # {"status": RET.DATAERR, "msg": "redis数据库异常"}
+        return jsonify(errno=RET.DATAERR, errmsg="redis数据库异常")
+    if real_image_code is None:
+        # 表示图片验证码没有或者过期
+        return jsonify(errno=RET.NODATA, errmsg="图片验证失效")
+    if real_image_code.lower() == image_code.lower():
+        # 表示用户填写错误
+        return jsonify(errno=RET.DATAERR, errmsg="图片验证码错误")
+
+    # 判断手机号是否存在，根据此结果是否产生短信验证码
+    # db.session(User)
+    try:
+        user = User.query.filter_by(mobile=mobile).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DATAERR, errmsg="mysql数据库异常")
+    if user is not None:
+        # 表示手机号已存在
+        return jsonify(errno=RET.DATAEXIST, errmsg="手机号已存在")
+    # 返回值
+    sms_code = "%6d" % random.randint(0, 999999)
+    # 保存真实短信验证码
+    try:
+        redis_store.setex("sms_code_%s" % mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DATAERR, errmsg="短信验证码异常")
+
+    # 发送短信
+    # try:
+    #     ccp = CCP()
+    #     result = ccp.send_template_sms(mobile, [sms_code, int(constants.SMS_CODE_REDIS_EXPIRES / 60)], 1)
+    # except Exception as e:
+    #     current_app.logger(e)
+    #     return jsonify(errno=RET.THIRDERR, errmsg="发送异常！")
+    # if result == 0:
+    #     # 返回值
+    #     return jsonify(errno=RET.OK, errmsg="发送成功，%s" % sms_code)
+    # else:
+    #     return jsonify(errno=RET.THIRDERR, errmsg="发送失败！")
+    return jsonify(errno=RET.OK, errmsg="发送成功，%s" % sms_code)
