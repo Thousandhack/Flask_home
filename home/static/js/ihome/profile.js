b4c57d65866ff5070b229233350dc7675c9ad45a
Thousandhack <1391053697@qq.com>
2020-08-02 20:21:50 +0800

    新增上传图片的前端代码

--- home/static/js/ihome/profile.js
+++ home/static/js/ihome/profile.js
@@ -1,8 +1,9 @@
 function showSuccessMsg() {
-    $('.popup_con').fadeIn('fast', function() {
-        setTimeout(function(){
-            $('.popup_con').fadeOut('fast',function(){}); 
-        },1000) 
+    $('.popup_con').fadeIn('fast', function () {
+        setTimeout(function () {
+            $('.popup_con').fadeOut('fast', function () {
+            });
+        }, 1000)
     });
 }
 
@@ -11,3 +12,29 @@ function getCookie(name) {
     return r ? r[1] : undefined;
 }
 
+
+$(document).ready(function () {
+    $("#form-avatar").submit(function (e) {
+        // 组织默认行为
+        e.preventDefault();
+        // 利用query.form.min.js提供akaxSubmit对表单进行异步提交
+        $(this).ajaxSubmit({
+            url: "/api/v1.0/users/avatar",
+            type: "post",
+            dataType: "json",
+            handlers: {
+                "X-CSRFToken": getCookie("csrf_token")
+            },
+            success: function (resp) {
+                if (resp.error == "0") {
+                    // 上传成功
+                    var avatarUrl = resp.data.avatar_url;
+                    $("#user-avatar").attr("src", avatarUrl);
+                } else {
+                    alert(resp.errmsg)
+                }
+
+            }
+        })
+    })
+});
