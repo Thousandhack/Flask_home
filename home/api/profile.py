f96bf32d01f5b42d3acd33f143d4cf5a5de2b595
Thousandhack <1391053697@qq.com>
2020-08-04 21:50:44 +0800

    新增改变用户名接口

--- home/api/profile.py
+++ home/api/profile.py
@@ -2,7 +2,7 @@
 from . import api
 from home.utils.commons import login_required
 from home.utils import constants
-from flask import g, current_app, jsonify, request
+from flask import g, current_app, jsonify, request, session
 from home import db
 from home.utils.response_code import RET
 from home.utils.image_storage import storage
@@ -47,4 +47,32 @@ def set_user_avatar():
     # 保存成功返回
     return jsonify(errno=RET.OK, errmsg="保存成功", data={"avatar_url": avatar_url})
 
-    #
+
+@api.route("/users/name", methods=["PUT"])
+@login_required
+def change_user_name():
+    """修改用户名"""
+    # 使用了login_required装饰器后，可以从g对象中获取用户user_id
+    user_id = g.user_id
+
+    # 获取用户想要设置的用户名
+    req_data = request.get_json()
+    if not req_data:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不完整")
+    name = req_data.get("name")
+    if not name:
+        return jsonify(errno=RET.PARAMERR, errmsg="名字不能为空")
+
+    # 保存用户昵称name,并同时name重复（利用数据库的唯一索引）
+    try:
+        # 注意这边的用户名设置为唯一
+        User.query.filter_by(id=user_id).update({"name": name})
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.OK, errmsg="设置用户错误")
+
+    # 修改session数据中的name字段
+    session['name'] = name
+    return jsonify(errno=RET.OK, errmsg="OK", data={"name": name})
