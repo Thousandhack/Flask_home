fec8e56723e1cd1604f01259ae5ec0e9e5320071
Thousandhack <1391053697@qq.com>
2020-08-05 23:13:04 +0800

    保存发布房源房屋的基本信息接口

--- home/api/houses.py
+++ home/api/houses.py
@@ -6,7 +6,7 @@ from home import db
 from home.utils.response_code import RET
 from home import redis_store
 from home.models import User
-from home.models import Area
+from home.models import Area, House, Facility
 import json
 
 
@@ -52,3 +52,119 @@ def get_area_info():
         current_app.logger.error(e)
     return resp_json, 200, {"Content-type": "application/json"}
     # return jsonify(errno=RET.OK, errmsg="OK", data=area_dict_list)
+
+
+@api.route("/houses/info", methods=["POST"])
+@login_required
+def save_house_info():
+    """
+    保存房屋的基本信息
+    前端发送过来的json数据
+    {
+        "title":"",
+        "price":"",
+        "area_id":"1",
+        "address":"",
+        "room_count":"",
+        "acreage":"",
+        "unit":"",
+        "capacity":"",
+        "beds":"",
+        "deposit":"",
+        "min_days":"",
+        "max_days":"",
+        "facility":["7","8"]
+    }
+    首先判断提交的数据是否全部都存在
+    判断金额是否正确
+    判断城区是否真的存在
+    判断设施是否都存在，会不会传的数据是不存在的id
+    保存房屋信息
+    返回房屋ID
+    :return:
+    """
+    # 获取数据
+    user_id = g.user_id
+    house_data = request.get_json()
+
+    title = house_data.get("title")  # 房屋名称标题
+    price = house_data.get("price")  # 房屋单价
+    area_id = house_data.get("area_id")  # 房屋所属城区的编号
+    address = house_data.get("address")  # 房屋地址
+    room_count = house_data.get("room_count")  # 房屋包含的房间数目
+    acreage = house_data.get("acreage")  # 房屋面积
+    unit = house_data.get("unit")  # 房屋布局（几室几厅)
+    capacity = house_data.get("capacity")  # 房屋容纳人数
+    beds = house_data.get("beds")  # 房屋卧床数目
+    deposit = house_data.get("deposit")  # 押金
+    min_days = house_data.get("min_days")  # 最小入住天数
+    max_days = house_data.get("max_days")  # 最大入住天数
+
+    # 校验参数
+    if not all(
+            [title, price, area_id, address, room_count, acreage, unit, capacity, beds, deposit, min_days, max_days]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不完整")
+
+    # 判断金额是否正确
+    try:
+        price = int(float(price) * 100)
+        deposit = int(float(deposit) * 100)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 判断城区id是否存在
+    try:
+        area = Area.query.get(area_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据库异常")
+
+    if area is None:
+        return jsonify(errno=RET.NODATA, errmsg="城区信息有误")
+
+    # 保存房屋信息
+    # 创建一个house对象
+    house = House(
+        user_id=user_id,
+        area_id=area_id,
+        title=title,
+        price=price,
+        address=address,
+        room_count=room_count,
+        acreage=acreage,
+        unit=unit,
+        capacity=capacity,
+        beds=beds,
+        deposit=deposit,
+        min_days=min_days,
+        max_days=max_days
+    )
+    # 处理房屋的设施信息
+    facility_ids = house_data.get("facility")
+
+    # 如果用户勾选了设施信息，再保存数据库
+    if facility_ids:
+        # ["7","8"]
+        try:
+            # select  * from ih_facility_info where id in []
+            facilities = Facility.query.filter(Facility.id.in_(facility_ids)).all()
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify(errno=RET.DBERR, errmsg="数据库异常")
+
+        if facilities:
+            # 表示有合法的设施数据
+            # 保存设施数据 保存多对多关系的数据
+            house.facilities = facilities
+
+    try:
+        db.session.add(house)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="保存数据失败")
+
+    # 保存数据成功
+    return jsonify(errno=RET.OK, errmsg="OK", data={"house_id": house.id})
