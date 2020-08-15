696556fbf9f3761a40fe5a6263a6726202a17abc
Thousandhack <1391053697@qq.com>
2020-08-15 23:53:09 +0800

    支付宝返回后的结果接口

--- home/api/pay.py
+++ home/api/pay.py
@@ -1,49 +1,94 @@
+# coding:utf-8
+
 from . import api
 from home.utils.commons import login_required
 from home.models import Order
-from flask import g
-from flask import current_app
-from flask import jsonify
+from flask import g, current_app, jsonify, request
 from home.utils.response_code import RET
+from alipay import AliPay
 from home.utils import constants
-from alipay import Alipay
 import os
 
 
 @api.route("/orders/<int:order_id>/payment", methods=["POST"])
-@login_required
+# @login_required
 def order_pay(order_id):
     """发起支付宝支付"""
-    user_id = g.user_id
-    # 判断订单状态
-    try:
-        order = Order.query.filter(Order.id == order_id, Order.user_id == user_id,
-                                   Order.status == "WAIT_PAYMENT").first()
-    except Exception as e:
-        current_app.logger.error(e)
-        return jsonify(errno=RET.DBERR, errmsg="数据库异常")
+    # user_id = g.user_id
 
-    if order is None:
-        return jsonify(errno=RET.NODATA, errmsg="订单数据不存在")
+    # 判断订单状态
+    # try:
+    #     order = Order.query.filter(Order.id == order_id, Order.user_id == user_id, Order.status == "WAIT_PAYMENT").first()
+    # except Exception as e:
+    #     current_app.logger.error(e)
+    #     return jsonify(errno=RET.DBERR, errmsg="数据库异常")
+    #
+    # if order is None:
+    #     return jsonify(errno=RET.NODATA, errmsg="订单数据有误")
+    app_private_key_path = open(os.path.join(os.path.dirname(__file__), "key/app_private_key.pem")).read()  # 私钥
+    alipay_public_key_path = open(os.path.join(os.path.dirname(__file__), "key/alipay_public_key.pem")).read()
+    # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
+
+    print(app_private_key_path)
+    # 创建支付宝sdk的工具对象
+    alipay_client = AliPay(
+        appid="2016081600258081",
+        app_notify_url=None,  # 默认回调url
+        app_private_key_path=app_private_key_path,  # 私钥
+        alipay_public_key_path=alipay_public_key_path,
+        # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
+        sign_type="RSA2",  # RSA 或者 RSA2
+        debug=False,  # 默认False
+    )
+
+    # 手机网站支付，需要跳转到https://openapi.alipaydev.com/gateway.do? + order_string
+    # api_alipay_trade_wap_pay
+    order_string = alipay_client.api_alipay_trade_page_pay(
+        out_trade_no=5,  # order.id,  # 订单编号
+        total_amount=str(100 / 100.0),  # 总金额
+        subject=u"爱家租房 %s",  # % order.id,  # 订单标题
+        return_url="http://127.0.0.1:5000/payComplete.html",  # 返回的连接地址
+        notify_url=None  # 可选, 不填则使用默认notify url
+    )
+
+    # 构建让用户跳转的支付连接地址
+    pay_url = constants.ALIPAY_URL_PREFIX + order_string
+    return jsonify(errno=RET.OK, errmsg="OK", data={"pay_url": pay_url})
+
+@api.route("/order/payment", methods=["PUT"])
+def save_order_payment_result():
+    """保存订单支付结果
+        支付宝返回后的结果接口
+    """
+    alipay_dict = request.form.to_dict()
+
+    # 对支付宝的数据进行分离  提取出支付宝的签名参数sign 和剩下的其他数据
+    alipay_sign = alipay_dict.pop("sign")
 
     # 创建支付宝sdk的工具对象
-    alipay_client = Alipay(
-        appid="2016101400681751",
-        app_notify_url=None,  # 默认回调地址
-        app_private_key_path=os.path.join(os.path.dirname(__file__), "key/app_private_key.pem"),  # 私钥
-        alipay_public_key_path=os.path.join(os.path.dirname(__file__), "key/app_public_key.pem"),
-        singn_type="RSA2",
-        debug=True,
+    alipay_client = AliPay(
+        appid="2016081600258081",
+        app_notify_url=None,  # 默认回调url
+        app_private_key_path=os.path.join(os.path.dirname(__file__), "keys/app_private_key.pem"),  # 私钥
+        alipay_public_key_path=os.path.join(os.path.dirname(__file__), "keys/alipay_public_key.pem"),
+        # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
+        sign_type="RSA2",  # RSA 或者 RSA2
+        debug=True  # 默认False
     )
 
-    # 手机网站支付
-    order_string = alipay_client.api_alipay_trade_way_pay(
-        out_trade_no=order.id,  # 订单编号
-        total_amount=str(order.amount / 100.0),
-        subject="测试订单",  # 订单标题
-        return_url="http://127.0.0.1:5000/orders.html",  # 返回的链接地址
-        notify_url=None  # 可选，不填则使用默认notify url
-    )
-    # 构建让用户跳转的支付
-    pay_url = constants.ALIPAY_URL_PREFIX + order_string
-    return jsonify(errno=RET.OK, errmsg="ok", data={"pay_url": pay_url})
+    # 借助工具验证参数的合法性
+    # 如果确定参数是支付宝的，返回True，否则返回false
+    result = alipay_client.verify(alipay_dict, alipay_sign)
+
+    if result:
+        # 修改数据库的订单状态信息
+        order_id = alipay_dict.get("out_trade_no")
+        trade_no = alipay_dict.get("trade_no")  # 支付宝的交易号
+        try:
+            Order.query.filter_by(id=order_id).update({"status": "WAIT_COMMENT", "trade_no": trade_no})
+            db.session.commit()
+        except Exception as e:
+            current_app.logger.error(e)
+            db.session.rollback()
+
+    return jsonify(errno=RET.OK, errmsg="OK")
